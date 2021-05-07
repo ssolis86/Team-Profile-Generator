c8d96a70b066a3d1411e620f2f02475ba572c7a4
Stephen Solis <ssolis8619@gmail.com>
2021-05-07 12:22:01 -0500

    setup Employee.js

--- index.js
+++ index.js
@@ -4,7 +4,29 @@ const fs = require('fs');
 const generateMarkdown = require('./utils/generateMarkdown');
 
 // TODO: Create an array of questions for user input
-const questions = [];
+inquirer
+    .prompt([
+        {
+            type: "input",
+            name: "managerName",
+            message: "Enter team manager's name: ",
+        },
+        {
+            type: "input",
+            name: "employeeId",
+            message: "Enter your employee ID:  ",
+        },
+        {
+            type: "input",
+            name: "email",
+            message: "Enter your email address: ",
+        },
+        {
+            type: "input",
+            name: "officeNumber",
+            message: "Enter your office telephone number: ",
+        },
+    ])
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
